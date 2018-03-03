a9402e2987673f2ed519623d1a04963e4285a280
d1m <dmitriy.finswim@gmail.com>
2018-03-03 18:18:41 +0700

    Server + DB (test)

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
+
 var pgp = require("pg-promise")(/*options*/);
 var db = pgp("postgres://client:qwerty@localhost:5432/project");
+var http = require("http");
 
-db.one("SELECT * from users")
-    .then(function (data) {
-        console.log(JSON.stringify(data));
-    })
-    .catch(function (error) {
-        console.log("ERROR:", error);
-    });
+http.createServer(function(request, response) {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    db.one("SELECT * from users")
+        .then(function (data) {
+            //response.write(data);
+            console.log(JSON.stringify(data));
+        })
+        .catch(function (error) {
+            //response.write("ERROR");
+            console.log("ERROR:", error);
+        });
+
+    response.end();
+}).listen(8888);
