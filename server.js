b185de0a3569fecfda607836607b5858b4c19be7
d1m <dmitriy.finswim@gmail.com>
2018-03-05 10:38:26 +0700

    Add .png scheme file

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var pgp = require("pg-promise")(/*options*/);
 var db = pgp("postgres://client:qwerty@localhost:5432/project");
 var http = require("http");
 
-http.createServer(function(request, response) {
+http.createServer(await function(request, response) {
     response.writeHead(200, {"Content-Type": "text/plain"});
     db.one("SELECT * from users")
         .then(function (data) {
