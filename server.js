6d0aaa372f0a47b1ff2012e52332faef4ceab262
d1m <dmitriy.finswim@gmail.com>
2018-03-08 14:55:43 +0700

    Add JSON convert + response

--- server.js
+++ server.js
@@ -7,18 +7,21 @@ var time;
 var speed;
 var productivity;
 var count;
+var json = '{"speed":"", "productivity":"", "count":"", "datetime":""}';
+json = JSON.parse(json);
 
-    http.createServer( function(request, response) {
+http.createServer( function(request, response) {
     response.writeHead(200, {"Content-Type": "text/html; charset=utf8"});
-
-        var index = fs.readFileSync('./index.html');
-        response.end(index);
+    var index = fs.readFileSync('./index.html');
+    response.end(index);
 
     db.many("select begin_date, end_date\n" +
         "from work_calendars\n" +
         "where descr != 'Working';")
         .then(function (data) {
             time = data;
+            time = JSON.stringify(time);
+            time = JSON.parse(time);
         })
         .catch(function (error) {
             //response.write("ERROR");
@@ -47,11 +50,19 @@ var count;
         .catch(function (error) {
             console.log("ERROR:", error);
         });
-    response.end();
+
     var i = 0;
     var t;
     if(count !== undefined && speed !== undefined && time !== undefined) {
         count = count[0].v;
+        productivity = productivity[0].value;
+        speed = speed[0].value;
+        json.speed = speed;
+        json.count = count;
+        json.productivity = productivity;
+        json.datetime = time;
+
+        response.end(json);
 
         while(i < count) {
             console.log("PAUSE TIME " + (i+1) + " (IN MINUTES)" );
@@ -62,11 +73,13 @@ var count;
             // добавить обработку случая, когда end_date < begin_date
             i++;
         }
-        speed = speed[0].value;
-        productivity = productivity[0].value;
+
         console.log("MAX SPEED: " + speed);
         console.log("TODAY'S PRODUCTIVITY: " + productivity);
+        console.log(json);
     }
+
+
 
 }).listen(8888);
 console.log('listening on port 8888...');
